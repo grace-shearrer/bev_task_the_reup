39b535108f3a1e14aea41dddb90b2f7ffca2bc6b
grace-shearrer <grace.shearrer@gmail.com>
2022-04-26 16:08:26 -0600

    training task and eff updated

--- design_eff_train_bevbit.R
+++ design_eff_train_bevbit.R
@@ -12,6 +12,10 @@ library(HH)
 # a variable delay (4-11 secs; mean 5 secs).
 # Each run contains 32 randomly presented blocks, consisting of 16 blocks of each taste-cue.
 
+# Prediction error (same stimulus timing and volumes; 30 events/run)
+# In 60% of the blocks (18 of 30), the visual cue associated with the palatable milkshake will be paired milkshake taste, followed by the rinse sequence.
+# In the other 40% (12 of 30) of blocks, the milkshake cue will be followed by receipt of the tasteless solution, creating a negative prediction error event (no rinse). (no paired-tasteless cue-taste in these runs)
+
 ## ------------------------------------------------------------------------
 
 #############################
@@ -22,13 +26,13 @@ delivery_time=4.0
 cue_time=2.0
 wait_time=2.0
 rinse_time=2.0
+runin_time = 8.0 
 
-tot_iti=cue_time+wait_time+rinse_time
-# Things that don't change
 water<-rep(0,16)
 sweet<-rep(1,16)
 all<-c(water, sweet)
 ntrials.total = 32
+
 
 dur = rep(delivery_time, ntrials.total)#length of stimulus of interest
 #min and max jitter values possible
@@ -36,19 +40,30 @@ min=4
 max=11
 
 #everything not jitter and not of interest (cue+wait+rinse)
-iti_inital = wait_time+cue_time+rinse_time
-iti_hard = rep(iti_inital+max, ntrials.total) #if using a random iti, you need to include this and the onsets into the loop
-ons.all = cumsum(c(0,dur+iti_hard))
-ons.all = ons.all[1:(ntrials.total)]
+iti_inital_sweet = cue_time+wait_time+rinse_time
+iti_inital_water = cue_time+wait_time
 
-run.length = max(ons.all)+100 #added just a ton of end time so the random jitter doesn't mess me up
+iti_hard_sweet = iti_inital_sweet+max #if using a random iti, you need to include this and the onsets into the loop
+iti_hard_water = iti_inital_miss+max #if using a random iti, you need to include this and the onsets into the loop
+
+iti_hard_comb_intm = iti_hard_sweet*all
+iti_hard_comb_intm = replace(iti_hard_comb_intm, iti_hard_comb_intm==0, iti_hard_miss)
+
+mockons.all = cumsum(c(0,dur+iti_hard_comb_intm))
+
+mockons.all = mockons.all[1:(ntrials.total)]
+
+
+run.length = max(mockons.all) 
+run.length
+
 tr = 1
 eff.size = 1
 
 # Things I'd like to save
 eff.val = rep(0, n.loop) #efficiency
 desmats = array(0,c(run.length, 3,  n.loop)) #all the model data
-ons.save = array(0,c(length(ons.all),3, n.loop)) #the onsets
+ons.save = array(0,c(ntrials.total,3, n.loop)) #the onsets
 
 Sys.time()->start;
 for (i in 1:n.loop){
@@ -59,38 +74,52 @@ for (i in 1:n.loop){
     keep.looking = max(length.repeats) > 2
   }
   
+ 
+  iti_hard_comb_intm = iti_inital_sweet*trial.type
+  iti_hard_comb_intm = replace(iti_hard_comb_intm, iti_hard_comb_intm==0, iti_inital_water)
+  
+  ons.all = cumsum(c(0,dur+iti_hard_comb_intm))
+  ons.all = ons.all[1:(ntrials.total)]
+  
+  
+  
   iti.uni = runif(1000, min, max) #randomly generate 1000 numbers between 1 and 9, mean is 5 this is the jitter
-  iti = rep(iti_inital, ntrials.total) #iti that is not random, wait, cue, ect
+
   vr <- c(0) ### an empty vector for jitter
   jit<-c(0)
-  for (j in 1:length(iti)) {#generating the jitter, randomly selecting a number from my distribution above
+  for (j in 1:length(iti_hard_comb_intm)) {#generating the jitter, randomly selecting a number from my distribution above
     jitter<-sample(iti.uni, 1)
     jit[j]<-round(jitter,0) #rounding jitter and adding it to a vector
-    vr[j] <- iti[j]+jit[j] # adding it to my non-random interval and then vector
+    vr[j] <- iti_hard_comb_intm[j]+jit[j] # adding it to my non-random interval and then vector
   }
-  ons.all = cumsum(c(0,dur+vr))
+  
+  ons.all = cumsum(c(0,vr))
   ons.all = ons.all[1:(ntrials.total)]
+  
   
   
   #taking the onsets and making them simulated activation
   sweet = specifydesign(ons.all[trial.type == 1], dur[trial.type == 1],
-                        run.length, tr,
-                       eff.size, conv = "double-gamma")
+                        tail(ons.all, n=1), tr,
+                        eff.size, conv = "double-gamma")
   
   water = specifydesign(ons.all[trial.type == 0], dur[trial.type == 0],
-                        run.length, tr,
-                        eff.size, conv = "double-gamma")
+                       tail(ons.all, n=1), tr,
+                       eff.size, conv = "double-gamma")
+  
   #save the simulated activation in a array (TRxContrast)
-  des.mat = cbind(rep(1, length(water)), water, sweet)
+  des.mat = cbind(rep(1, length(sweet)), water, sweet)
   #ons.save= cbind(ons.save,trial.type)
   #ons.save[,,i]=
   # making the contrast this is sweet>water
-  con = c(0, 1, -1) 
+  con = c(0, -1, 1) 
   #solving for the efficiency matrix
   eff.val[i] = 1/(t(con)%*%solve(t(des.mat)%*%des.mat)%*%con)
   ons.save[,,i]=c(ons.all,jit,trial.type)
   #creating an array adding des.mat to the efficiency matrix
-  desmats[,,i] = des.mat
+  X = array('NA',c(dim(desmats)[1] - dim(des.mat)[1], 3)) # this is to get the matrix the same size as the desmats, need this because the lack of rinse and the jitter can make them different
+  Y = rbind(des.mat, X)
+  desmats[,,i] = Y
 }
 print(Sys.time()-start)
 
@@ -98,25 +127,24 @@ print(Sys.time()-start)
 par(mfrow = c(2, 1), mar = c(4, 3, 2, 1))
 # finding the most eff matrix to plot all of the columns simulated activation for each TR
 water.best = desmats[,2,which(eff.val == max(eff.val))]
+water.best <- water.best[!is.na(water.best)]
+
 sweet.best = desmats[,3,which(eff.val == max(eff.val))]
-#unsweet.best = desmats[,4,which(eff.val == max(eff.val))]
+sweet.best <- sweet.best[!is.na(sweet.best)]
 
 plot(water.best, type = 'l', lwd = 2, col = 'red', xlab = "TR", 
      ylab = '', ylim = c(min(c(water.best, sweet.best)), 1.3),
      main = "Highest Efficiency")
 lines(sweet.best, lwd = 2, col = 'cyan')
-#lines(unsweet.best, lwd = 2, col = 'black')
 
 water.worst = desmats[,2,which(eff.val == min(eff.val), arr.ind = TRUE)]
 sweet.worst = desmats[,3,which(eff.val == min(eff.val))]
-#unsweet.worst = desmats[,4,which(eff.val == min(eff.val))]
 
 plot(water.worst, type = 'l', lwd = 2, col = 'red', xlab = "TR", 
      ylab = '', ylim = c(min(c(water.worst, sweet.worst)), 1.3),
      main = "Lowest Efficiency")
 lines(sweet.worst, lwd = 2, col = 'cyan')
-#lines(unsweet.worst, lwd = 2, col = 'black')
-legend('topleft', c("sweet 1", "water 0"), col = c("cyan", "black", "red"), lwd = c(2,1), bty = 'n')
+legend('topleft', c("sweet 1", "water 0"), col = c("cyan", "red"), lwd = c(2,1), bty = 'n')
 
 #desmats TRxcontrast_activationxloops
 #there are n.loops of matrices, with timepoint rows and contrast columns
@@ -133,6 +161,6 @@ fake.data = rnorm(length(water))
 mod.fake = lm(fake.data ~ water.best + sweet.best)
 vif(mod.fake)
 
-write.table(ons.save[,,best][,1], "/Users/gracer/Documents/Bev_Task_reup/onset_files/train/onsets_run02", row.names = F,col.names = F, sep="\t")
-write.table(ons.save[,,best][,2], "/Users/gracer/Documents/Bev_Task_reup/onset_files/train/jitter_run02", row.names = F, col.names = F, sep="\t")
-write.table(ons.save[,,best][,3], "/Users/gracer/Documents/Bev_Task_reup/onset_files/train/conds_run02", row.names = F, col.names = F, sep="\t")
+write.table(ons.save[,,best][,1], "/Users/gracer/Documents/Bev_Task_reup/onset_files/train/onsets_run01", row.names = F,col.names = F, sep="\t")
+write.table(ons.save[,,best][,2], "/Users/gracer/Documents/Bev_Task_reup/onset_files/train/jitter_run01", row.names = F, col.names = F, sep="\t")
+write.table(ons.save[,,best][,3], "/Users/gracer/Documents/Bev_Task_reup/onset_files/train/conds_run01", row.names = F, col.names = F, sep="\t")
--- training_task_working.py
+++ training_task_working.py
@@ -1,22 +1,38 @@
-# taste task. 4/5/2018
-#this is for BEVBITS (formerly Juice)
-#water is pump 0
-#sweet is pump 1
-#notsweet is pump 2
-#rinse is pump 3
-#TR 2 sec
-#the pkl file contains all study data as a back up including what files were used, useful for sanity checks
-#the csv file is easier to read
-#the log file also has onsets, but it has the time from when the .py file was initalized more accurate should be used for analysis
+# taste task. 4/25/2022
+# New task
+# water is pump 0
+# milkshake aka sweet is pump 1
+# rinse is pump 2
+
+# TR 2 sec
+# the pkl file contains all study data as a back up including what files were used, useful for sanity checks
+# the csv file is easier to read
+# the log file also has onsets, but it has the time from when the .py file was initalized more accurate should be used for analysis
+'''
+This expects to be in the Documents folder. It also expects a folder in the Documents folder called "Train_Output". If you want to change these you need to change the paths
+milkshake: a milkshake-paired cue (2 sec), followed by a blank screen, and
+the paired milkshake will then be delivered (4 sec; 3mL).
+followed by a waiting period (2 secs) (blank),
+a rinse with the neutral solution (2 sec; 0.7mL),
+a variable delay (4-11 secs; mean 5 secs).
+h20: tasteless-paired 2 sec, blank
+tasteless admin (4 sec; 3mL)
+a variable delay (4-11 secs; mean 5 secs).
+Each run contains 32 randomly presented blocks, consisting of 16 blocks of each taste-cue.
+'''
+
+
+
 from psychopy import visual, core, data, gui, event, data, logging
 import csv
 import time
 import serial
-import numpy as N 
+import numpy as np
 import sys,os,pickle
 import datetime
 import exptutils
 from exptutils import *
+import os
 
 monSize = [800, 600]
 info = {}
@@ -24,8 +40,8 @@ info['fullscr'] = False
 info['port'] = '/dev/tty.usbserial'
 info['participant'] = 'test'
 info['run']='run01'
-info['session']='pre'
-info['flavor']='' #Either CO or SL
+#info['session']='pre'
+#info['flavor']='' #Either CO or SL
 info['computer']=(os.getcwd()).split('/')[2]
 dlg = gui.DlgFromDict(info)
 if not dlg.OK:
@@ -51,13 +67,15 @@ subdata['SS']={}
 subdata['broke_on_trial']={}
 subdata['simulated_response']=False
 #
-subdata['onset']='/Users/'+info['computer']+'/Documents/bevbit_task/onset_files/'+info['session']+'/onsets_'+info['run']
-subdata['jitter']='/Users/'+info['computer']+'/Documents/bevbit_task/onset_files/'+info['session']+'/jitter_'+info['run']
-subdata['conds']='/Users/'+info['computer']+'/Documents/bevbit_task/onset_files/'+info['session']+'/conds_'+info['run']
+subdata['onset'] = os.path.join('Users','%s'%info['computer'],'/Documents/bevbit_task/onset_files/train/onsets_%s'%info['run'])
+subdata['jitter'] = os.path.join('Users','%s'%info['computer'],'/Documents/bevbit_task/onset_files/train/jitter_%s'%info['run'])
+subdata['conds'] = os.path.join('Users','%s'%info['computer'],'/Documents/bevbit_task/onset_files/train/conds_%s'%info['run'])
+
+
 subdata['quit_key']='q'
 
 #######################################
-dataFileName='/Users/'+info['computer']+'/Documents/Output/%s_%s_%s_%s_subdata.log'%(info['participant'],info['run'],info['session'],subdata['datestamp'])
+dataFileName = os.path.join('Users','%s'%info['computer'],'/Documents/Train_Output/%s_%s_%s_%s_subdata.log'%(info['participant'],info['run'],info['session'],subdata['datestamp']))
 logging.console.setLevel(logging.INFO)
 logfile=logging.LogFile(dataFileName,level=logging.DATA)
 ratings_and_onsets = []
@@ -75,36 +93,35 @@ if not ser.isOpen():
 
 time.sleep(1)
 
+
 #global settings
 diameter=26.59
 mls_sweet=3.0
-mls_unsweet=3.0
 mls_H2O=3.0
-mls_rinse=1.0
-delivery_time=6.0
-cue_time=1.0
+mls_rinse=0.7
+delivery_time=4.0
+cue_time=2.0
 wait_time=2.0
-rinse_time=3.0
+rinse_time=2.0
+runin_time = 8.0 # this is the time before the task begins to correct for intial brightness, delete the first 4 volumes (or however many per TR)
 
 str='\r'
 rate_sweet = mls_sweet*(3600.0/delivery_time)  # mls/hour 300
-rate_unsweet = mls_unsweet*(3600.0/delivery_time)  # mls/hour 300
 rate_H2O = mls_H2O*(3600.0/delivery_time)  # mls/hour 300
 rate_rinse = mls_rinse*(3600.0/rinse_time)  # mls/hour 300
 
 print('rate for sweet %f'%rate_sweet)
 print('rate for H2O %f'%rate_H2O)
 print('rate for rinse %f'%rate_rinse)
-print('rate for unsweet %f'%rate_unsweet)
+#print('rate for unsweet %f'%rate_unsweet)
 
-pump_setup = ['0VOL ML\r', '1VOL ML\r', '2VOL ML\r','3VOL ML\r']
-pump_phases=['0DIA%.2fMH\r'%diameter,'1DIA%.2fMH\r'%diameter, '2DIA%.2fMH\r'%diameter,'3DIA%.2fMH\r'%diameter,
-'0CLDINF\r','1CLDINF\r','2CLDINF\r','3CLDINF\r'
-'0PHN01\r','1PHN01\r', '2PHN01\r','3PHN01\r'
-'0DIRINF\r','1DIRINF\r','2DIRINF\r','3DIRINF\r'
-'0RAT%iMH\r'%rate_H2O,'1RAT%iMH\r'%rate_sweet,'2RAT%iMH\r'%rate_unsweet,'3RAT%iMH\r'%rate_rinse,
-'0VOL%i%s'%(mls_H2O,str), '1VOL%i%s'%(mls_sweet,str),'2VOL%i%s'%(mls_unsweet,str),'3VOL%i%s'%(mls_rinse,str)
-]
+pump_setup = ['0VOL ML\r', '1VOL ML\r', '2VOL ML\r']
+pump_phases=['0DIA%.2fMH\r'%diameter,'1DIA%.2fMH\r'%diameter, '2DIA%.2fMH\r'%diameter,
+'0CLDINF\r','1CLDINF\r','2CLDINF\r',
+'0PHN01\r','1PHN01\r', '2PHN01\r',
+'0DIRINF\r','1DIRINF\r','2DIRINF\r',
+'0RAT%iMH\r'%rate_H2O,'1RAT%iMH\r'%rate_sweet,'2RAT%iMH\r'%rate_rinse,
+'0VOL%i%s'%(mls_H2O,str), '1VOL%i%s'%(mls_sweet,str),'2VOL%i%s'%(mls_rinse,str)]
 
 
 for c in pump_setup:
@@ -128,7 +145,7 @@ def show_stim(stim, seconds):
     for frame in range(60 * seconds):
         stim.draw()
         win.flip()
-        
+
 def check_for_quit(subdata,win):
     k=event.getKeys()
     print 'checking for quit key %s'%subdata['quit_key']
@@ -151,10 +168,11 @@ def tastes(params):
 win = visual.Window(monSize, fullscr=info['fullscr'],
                     monitor='testMonitor', units='deg')
 visual_stim=visual.ImageStim(win, image=N.zeros((300,300)), size=(0.75,0.75),units='height')
+
 # STIMS
 fixation_text = visual.TextStim(win, text='+', pos=(0, 0), height=2)
 
-scan_trigger_text = visual.TextStim(win, text='During this scan you are going to get tastes of water and 2 beverages. Please pay attention to the logos and the tastes', pos=(0, 0))
+scan_trigger_text = visual.TextStim(win, text='During this scan you are going to get tastes of water and milkshake. Please pay attention to the logos and the tastes', pos=(0, 0))
 
 tastes(pump_phases)
 
@@ -176,37 +194,34 @@ g=open(subdata['jitter'],'r')
 y = g.readlines()
 for i in y:
     jitter.append(i.strip())
-    
+
 jitter=[float(i) for i in jitter]
 print(jitter, 'jitter')
 
-trialcond=N.loadtxt(subdata['conds'], dtype='int')
+trialcond=np.loadtxt(subdata['conds'], dtype='int')
 print(trialcond,'trial conditions')
 
 ntrials=len(trialcond)
 pump=N.zeros(ntrials)
-#    pump zero is water, pump 1 is sweet, pump 2 is unsweet
+#    pump zero is water, pump 1 is milkshake, pump 2 is rinse
 #    these need to match the onset files!
 
 pump[trialcond==0]=0 #water pump
 pump[trialcond==1]=1 #sweet pump
-pump[trialcond==2]=2 #unsweet pump
+#pump[trialcond==2]=2 #unsweet pump
 
-if info['flavor']=='CO':
-    stim_images=['water.jpg','CO.jpg','UCO.jpg']
-else:
-    stim_images=['water.jpg', 'SL.jpg', 'USL.jpg']
+stim_images=['water.jpg','milk.jpg']
 
 subdata['trialdata']={}
-myfile = open('/Users/'+info['computer']+'/Documents/Output/BBX_training_subdata_%s.csv'%datestamp.format(**info), 'wb')
+myfile = open(os.path.join('Users','%s'%info['computer'],'/Documents/Output/taste_reup_training_subdata_%s.csv'%datestamp.format(**info)),'wb')
 
-            
+
 """
     The main run block!
 """
 
 def run_block():
-    
+
     # Await scan trigger
     while True:
         scan_trigger_text.draw()
@@ -219,9 +234,9 @@ def run_block():
     clock=core.Clock()
     t = clock.getTime()
     ratings_and_onsets.append(['fixation',t])
-    show_stim(fixation_text, 8)  # 8 sec blank screen with fixation cross
+    show_stim(fixation_text, runin_time)  # blank screen with fixation cross
     t = clock.getTime()
-    clock.reset()
+    # clock.reset()
     ratings_and_onsets.append(['start',t])
     #logging.log(logging.DATA, "start")
     for trial in range(ntrials):
@@ -232,22 +247,22 @@ def run_block():
             for row in ratings_and_onsets:
                 wr.writerow(row)
             sys.exit()
-        
+
         trialdata={}
         trialdata['onset']=onsets[trial]
         logging.log(logging.DATA, "onset of trial =%f"%trialdata['onset'])
         ratings_and_onsets.append(["onset =%f"%trialdata['onset']])
         logging.flush()
-        
+
         visual_stim.setImage(stim_images[trialcond[trial]])#set which image appears
-        print trial
-        print 'condition %d'%trialcond[trial]
-        print 'showing image: %s'%stim_images[trialcond[trial]]
-        
-        
+        print(trial)
+        print('condition %d'%trialcond[trial])
+        print('showing image: %s'%stim_images[trialcond[trial]])
+
+
         visual_stim.draw()#setting the image
-        
-        
+
+
         while clock.getTime()<trialdata['onset']:
             pass
         win.flip()#showing the image of the stimulus
@@ -255,7 +270,7 @@ def run_block():
         logging.log(logging.DATA, "image=%s"%stim_images[trialcond[trial]])
         ratings_and_onsets.append(["image=%s"%stim_images[trialcond[trial]],t])
         logging.flush()
-        
+
         while clock.getTime()<(trialdata['onset']+cue_time):#show the image
             pass
 
@@ -263,21 +278,19 @@ def run_block():
         message.draw()
         win.flip()#making the blank screen appear
 
-        print 'injecting via pump at address %d'%pump[trial]
-        
-#        logging.log(logging.DATA,"injecting via pump at address %d"%pump[trial])
-#        logging.flush()
+        print('injecting via pump at address %d'%pump[trial])
+
         t = clock.getTime()
         ratings_and_onsets.append(["injecting via pump at address %d"%pump[trial], t])
         logging.log(logging.DATA,"injecting via pump at address %d"%pump[trial])
         logging.flush()
         ser.write('%dRUN\r'%pump[trial])
         logging.log(logging.DATA,"post injecting via pump at address %d"%pump[trial])
-        
-        
+
+
         while clock.getTime()<(trialdata['onset']+cue_time+delivery_time):
             pass
-        
+
         message=visual.TextStim(win, text='+', pos=(0, 0), height=2)#this lasts throught the wait
         message.draw()
         win.flip()
@@ -285,51 +298,51 @@ def run_block():
         t = clock.getTime()
         ratings_and_onsets.append(["wait", t])
         logging.flush()
-        
+
         trialdata['dis']=[ser.write('0DIS\r'),ser.write('1DIS\r')]
         print(trialdata['dis'])
 
-        
+
         while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time):
             pass
-        
+
         if pump[trial]==0:
-            message=visual.TextStim(win, text='+', pos=(0, 0), height=2)#lasts through the jitter 
+            message=visual.TextStim(win, text='+', pos=(0, 0), height=2)#lasts through the jitter
             message.draw()
             win.flip()
             logging.log(logging.DATA, "NO RINSE")
             logging.flush()
-            
+
             t = clock.getTime()
             ratings_and_onsets.append(["jitter", t])
-            
 
-            
-            while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time+rinse_time+jitter[trial]):
+
+
+            while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time+jitter[trial]):
                 pass
-        
+
             t = clock.getTime()
             ratings_and_onsets.append(['end time', t])
             logging.log(logging.DATA,"finished")
             logging.flush()
             subdata['trialdata'][trial]=trialdata
-            
+
         else:
-            message=visual.TextStim(win, text=' ', pos=(0, 0), height=2)#this lasts throught the rinse 
+            message=visual.TextStim(win, text=' ', pos=(0, 0), height=2)#this lasts throught the rinse
             message.draw()
             win.flip()
-                
+
             print 'injecting rinse via pump at address %d'%0
             t = clock.getTime()
             ratings_and_onsets.append(['injecting rinse via pump at address %d'%0, t])
-            ser.write('%dRUN\r'%3)
+            ser.write('%dRUN\r'%2)
             logging.log(logging.DATA, "RINSE")
             logging.flush()
-        
+
             while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time+rinse_time):
                 pass
 
-            message=visual.TextStim(win, text='+', pos=(0, 0), height=2)#lasts through the jitter 
+            message=visual.TextStim(win, text='+', pos=(0, 0), height=2)#lasts through the jitter
             message.draw()
             win.flip()
             logging.log(logging.DATA,"Start Jitter")
@@ -337,12 +350,12 @@ def run_block():
             t = clock.getTime()
             ratings_and_onsets.append(["jitter", t])
 
-            
+
 
             while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time+rinse_time+jitter[trial]):
                 pass
             t = clock.getTime()
-            
+
             ratings_and_onsets.append(['end time', t])
             logging.log(logging.DATA,"finished")
             logging.flush()
@@ -354,7 +367,7 @@ def run_block():
 run_block()
 
 subdata.update(info)
-f=open('/Users/'+info['computer']+'/Documents/Output/BBX_subdata_%s.pkl'%datestamp,'wb')
+f=open('/Users/'+info['computer']+'/Documents/Output/taste_reup_subdata_%s.pkl'%datestamp,'wb')
 pickle.dump(subdata,f)
 f.close()
 
